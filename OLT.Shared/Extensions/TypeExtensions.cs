227841bd54d1afa1cf1ec9fd65a28fd107212c50
Chris Straw <chris.straw@outerlimitstech.com>
2021-07-27 08:52:31 -0400

    #10 - Added Addl Extensions

--- OLT.Shared/Extensions/TypeExtensions.cs
+++ OLT.Shared/Extensions/TypeExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Linq;
 using System.Reflection;
 
 // ReSharper disable once CheckNamespace
@@ -8,8 +9,8 @@ namespace System
     public static class TypeExtensions
     {
         /// <summary>
-        /// Determines if a type implements the given interface.
-        /// this.GetType().Implements<IDisposable>()
+        /// Determines if a type implements the <typeparamref name="TInterface"/> interface.
+        /// this.GetType().Implements<IMyInterface>()
         /// </summary>
         /// <typeparam name="TInterface">Interface</typeparam>
         /// <param name="type">Extends <see cref="Type"/>.</param>
@@ -22,6 +23,25 @@ namespace System
                 throw new InvalidOperationException("Only interfaces can be implemented.");
 
             return (interfaceType.IsAssignableFrom(type));
+        }
+
+        /// <summary>
+        /// Determines if a type implements the <typeparamref name="TInterface"/> interface.
+        /// this.GetType().Implements(typeof(IMyInterface<>))
+        /// </summary>
+        /// <typeparam name="TInterface">Interface</typeparam>
+        /// <param name="type">Extends <see cref="Type"/>.</param>
+        /// <returns>True if type implements interface</returns>
+        public static bool Implements<TInterface>(this Type type, TInterface @interface) where TInterface : class
+        {
+            if (((@interface as Type) == null) || !(@interface as Type).IsInterface)
+                throw new ArgumentException("Only interfaces can be 'implemented'.");
+
+            //return (@interface as Type).IsAssignableFrom(type);
+
+            return type.GetInterfaces()
+                .Where(i => i.IsGenericType)
+                .Any(i => i.GetGenericTypeDefinition() == (@interface as Type));
         }
 
         public static bool EmbeddedResourceToFile(this Assembly assembly, string resourceName, string fileName)
