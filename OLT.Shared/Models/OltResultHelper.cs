c6d0382ba48489f6e4db4c04e6d9f2facf0580b4
Chris Straw <chris.straw@outerlimitstech.com>
2021-08-13 17:22:38 -0400

    Rule Result Tweaks

---
+++ OLT.Shared/Models/OltResultHelper.cs
@@ -0,0 +1,8 @@
+﻿namespace OLT.Core
+{
+    public static class OltResultHelper
+    {
+        public static IOltResult Success => new OltResultSuccess();
+        public static IOltResultValidation Valid => new OltResultValid();
+    }
+}
---
+++ OLT.Shared/Models/OltResultValid.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace OLT.Core
+{
+    public class OltResultValid : IOltResultValidation
+    {
+        public virtual bool Success => true;
+        public List<IOltValidationError> Results => new List<IOltValidationError>();
+    }
+}
