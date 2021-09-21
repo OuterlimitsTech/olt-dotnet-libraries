3987c6c26ebf34919e5597e507322f617dea2cd5
Chris Straw <chris.straw@outerlimitstech.com>
2021-09-21 08:30:13 -0400

    code coverage

---
+++ OLT.Libraries.UnitTest/OLT.Shared/NgxLoggerMessage/HelperNgxExceptionTest.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Faker;
+using OLT.Core;
+
+namespace OLT.Libraries.UnitTest.OLT.Shared.NgxLoggerMessage
+{
+    public class HelperNgxExceptionTest
+    {
+        public HelperNgxExceptionTest()
+        {
+            UnixTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+            Stack = new List<OltNgxLoggerStack>
+            {
+                new OltNgxLoggerStack
+                {
+                    ColumnNumber = Faker.RandomNumber.Next(3),
+                    LineNumber = Faker.RandomNumber.Next(4),
+                    FileName = Faker.Lorem.GetFirstWord(),
+                    FunctionName = Faker.Lorem.GetFirstWord(),
+                    Source = Faker.Lorem.Paragraph()
+                }
+            };
+
+
+            Result = new Dictionary<string, string>
+            {
+                { "Name", Faker.Name.FullName(NameFormats.WithSuffix) },
+                { "AppId", Faker.Lorem.GetFirstWord() },
+                { "User", Faker.Internet.UserName() },
+                { "Time", DateTimeOffset.FromUnixTimeMilliseconds(UnixTime).ToISO8601() },
+                { "Url", Faker.Internet.Url() },
+                { "Status", Faker.RandomNumber.Next(200, 600).ToString() },
+                { "Stack", string.Join(Environment.NewLine,  Stack.Select(s => $"{s}{Environment.NewLine}{Environment.NewLine}").ToList()) }
+            };
+
+            Detail = new OltNgxLoggerDetail
+            {
+                Id = Faker.Lorem.GetFirstWord(),
+                AppId = Result["AppId"],
+                Message = Faker.Lorem.Sentence(),
+                Name = Result["Name"],
+                Status = Result["Status"],
+                Time = UnixTime,
+                Url = Result["Url"],
+                User = Result["User"],
+                Stack = Stack
+            };
+        }
+
+
+        public long UnixTime { get; }
+        public Dictionary<string, string> Result { get; }
+        public List<OltNgxLoggerStack> Stack { get; }
+        public OltNgxLoggerDetail Detail { get; }
+
+
+        public OltNgxLoggerMessage BuildMessage(DateTimeOffset dt, OltNgxLoggerLevel level, OltNgxLoggerDetail detail)
+        {
+            var msg = new OltNgxLoggerMessage
+            {
+                Message = Faker.Lorem.Sentence(),
+                Timestamp = dt,
+                Level = level,
+                FileName = Faker.Lorem.GetFirstWord(),
+                LineNumber = Faker.RandomNumber.Next(1000, 4000).ToString(),
+            };
+
+            if (detail != null)
+            {
+                msg.Additional = new List<List<OltNgxLoggerDetail>>
+                {
+                    new List<OltNgxLoggerDetail>
+                    {
+                        detail
+                    }
+                };
+            }
+
+            Result.Add("Username", msg.Username);
+            Result.Add("Level", msg.Level.ToString());
+            Result.Add("LineNumber", msg.LineNumber);
+            Result.Add("FileName", msg.FileName);
+            Result.Add("Timestamp", msg.Timestamp?.ToISO8601());
+
+            return msg;
+        }
+
+    }
+}
---
+++ OLT.Libraries.UnitTest/OLT.Shared/NgxLoggerMessage/NgxExceptionTests.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using OLT.Core;
+using OLT.Libraries.UnitTest.Abstract;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace OLT.Libraries.UnitTest.OLT.Shared.NgxLoggerMessage
+{
+    public class NgxExceptionTests : BaseTest
+    {
+
+        public NgxExceptionTests(ITestOutputHelper output) : base(output)
+        {
+
+        }
+
+        public static IEnumerable<object[]> NgxLoggerMessageData =>
+            new List<object[]>
+            {
+                new object[] { new HelperNgxExceptionTest(), OltNgxLoggerLevel.Error, true },
+                new object[] { new HelperNgxExceptionTest(), OltNgxLoggerLevel.Fatal, true },
+                new object[] { new HelperNgxExceptionTest(), OltNgxLoggerLevel.Info, true },
+
+                new object[] { new HelperNgxExceptionTest(), OltNgxLoggerLevel.Error, false },
+                new object[] { new HelperNgxExceptionTest(), OltNgxLoggerLevel.Fatal, false },
+                new object[] { new HelperNgxExceptionTest(), OltNgxLoggerLevel.Info, false },
+            };
+
+        private Dictionary<string, string> ToDictionary(IDictionary data)
+        {
+            var dict = new Dictionary<string, string>();
+            foreach (DictionaryEntry dictionaryEntry in data)
+            {
+                dict.Add(dictionaryEntry.Key.ToString() ?? string.Empty, dictionaryEntry.Value?.ToString());
+                Logger.Debug("{key} = {value}", dictionaryEntry.Key, dictionaryEntry.Value);
+            }
+            return dict;
+        }
+
+
+        [Theory]
+        [MemberData(nameof(NgxLoggerMessageData))]
+        public void NgxLoggerMessage(HelperNgxExceptionTest data, OltNgxLoggerLevel level, bool loadDetail)
+        { 
+            var dt = DateTimeOffset.Now;
+            
+            var msg = data.BuildMessage(dt, level, loadDetail ? data.Detail : null);
+
+            if (!loadDetail && level == OltNgxLoggerLevel.Fatal)
+            {
+                msg.Additional.Add(new List<OltNgxLoggerDetail>());
+            }
+
+            var exception = msg.ToException();
+            var exceptionMessage = msg.Additional.FirstOrDefault()?.FirstOrDefault()?.Message ?? msg.Message;
+            Assert.Equal(exceptionMessage, exception.Message);
+
+            var dict = ToDictionary(exception.Data);
+            if (loadDetail)
+            {
+                Assert.Collection(dict,
+                    item => Assert.Equal(data.Result["Name"], item.Value),
+                    item => Assert.Equal(data.Result["AppId"], item.Value),
+                    item => Assert.Equal(data.Result["User"], item.Value),
+                    item => Assert.Equal(data.Result["Time"], item.Value),
+                    item => Assert.Equal(data.Result["Url"], item.Value),
+                    item => Assert.Equal(data.Result["Status"], item.Value),
+                    item => Assert.Equal(data.Result["Stack"], item.Value),
+                    item => Assert.Equal(data.Result["Username"], item.Value),
+                    item => Assert.Equal(data.Result["Level"], item.Value),
+                    item => Assert.Equal(data.Result["LineNumber"], item.Value),
+                    item => Assert.Equal(data.Result["FileName"], item.Value),
+                    item => Assert.Equal(data.Result["Timestamp"], item.Value)
+                );
+
+                return;
+            }
+
+
+            Assert.Collection(dict,
+                item => Assert.Equal(data.Result["Username"], item.Value),
+                item => Assert.Equal(data.Result["Level"], item.Value),
+                item => Assert.Equal(data.Result["LineNumber"], item.Value),
+                item => Assert.Equal(data.Result["FileName"], item.Value),
+                item => Assert.Equal(data.Result["Timestamp"], item.Value)
+            );
+
+        }
+
+
+        public static IEnumerable<object[]> NgxLoggerDetailData =>
+            new List<object[]>
+            {
+                new object[] { new HelperNgxExceptionTest() },
+                new object[] { new HelperNgxExceptionTest() },
+                new object[] { new HelperNgxExceptionTest() },
+            };
+
+        [Theory]
+        [MemberData(nameof(NgxLoggerDetailData))]
+        public void NgxLoggerDetail(HelperNgxExceptionTest data)
+        {
+            var exception = data.Detail.ToException();
+            Assert.Equal(data.Detail.Message, exception.Message);
+            Assert.Equal(data.Detail.Id, exception.Source);
+            var dict = ToDictionary(exception.Data);
+
+            Assert.Collection(dict,
+                item => Assert.Equal(data.Result["Name"], item.Value),
+                item => Assert.Equal(data.Result["AppId"], item.Value),
+                item => Assert.Equal(data.Result["User"], item.Value),
+                item => Assert.Equal(data.Result["Time"], item.Value),
+                item => Assert.Equal(data.Result["Url"], item.Value),
+                item => Assert.Equal(data.Result["Status"], item.Value),
+                item => Assert.Equal(data.Result["Stack"], item.Value)
+            );
+        }
+
+
+    }
+}
