52022de797b52d5762dce983cc3973d93055f789
Chris Straw <chris.straw@outerlimitstech.com>
2021-10-27 20:05:52 -0400

    removed IOltConfigManager

--- OLT.Shared/Services/OltHostServiceBase.cs
+++ OLT.Shared/Services/OltHostServiceBase.cs
@@ -7,16 +7,13 @@ namespace OLT.Core
         private IOltEnvironment _environment;
 
         protected OltHostServiceBase(
-            IOltConfigManager configManager,
             IOltLogService loggingService)
         {
-            ConfigManager = configManager;
             LoggingService = loggingService;
         }
 
         public abstract string ResolveRelativePath(string filePath);
         public abstract string EnvironmentName { get; }
-        public virtual IOltConfigManager ConfigManager { get; }
         public virtual IOltEnvironment Environment => _environment ?? (_environment = new OltEnvironment(EnvironmentName));
         public virtual IOltLogService LoggingService { get; }
 
