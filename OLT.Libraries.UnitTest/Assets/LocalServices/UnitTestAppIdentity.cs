4fc16d4372807e4b886eceef188e1712014cb993
Chris Straw <chris.straw@outerlimitstech.com>
2022-01-27 18:21:38 -0500

    WIP

--- OLT.Libraries.UnitTest/Assets/LocalServices/UnitTestAppIdentity.cs
+++ OLT.Libraries.UnitTest/Assets/LocalServices/UnitTestAppIdentity.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using System.Security.Principal;
 using OLT.Core;
@@ -27,6 +28,7 @@ namespace OLT.Libraries.UnitTest.Assets.LocalServices
         }
 
     }
+
     public class OltUnitTestAppIdentity : OltIdentity
     {
         public const string StaticUser = "UnitTest";
@@ -60,5 +62,117 @@ namespace OLT.Libraries.UnitTest.Assets.LocalServices
             return true;
         }
 
+    }
+
+    public enum TestSecurityRoles
+    {
+        [Code("role-one")]
+        RoleOne,
+
+        [Code("role-two")]
+        RoleTwo,
+
+        [Code("role-three")]
+        RoleThree,
+    }
+
+    public enum TestSecurityPermissions
+    {
+        [Code("perm-one")]
+        PermissionOne,
+
+        [Code("perm-two")]
+        PermissionTwo,
+
+        [Code("perm-three")]
+        PermissionThree,
+    }
+
+
+    public class FakeUserIdentity
+    {
+
+        public FakeUserIdentity() 
+        {
+            NameIdentifier = Faker.Internet.UserName();
+            Email = Faker.Internet.Email();
+            UserPrincipalName = Faker.RandomNumber.Next(1000, 9000);
+            Phone = Faker.Phone.Number();
+            MobilePhone = Faker.Phone.Number();
+
+            PersonName = new OltPersonName
+            {
+                First = Faker.Name.First(),
+                Middle = Faker.Name.Middle(),
+                Last = Faker.Name.Last(),
+            };
+        }
+        private List<Claim> _claims = new List<Claim>();
+        public string NameIdentifier { get; }
+        public string Email {  get; }
+        public int UserPrincipalName { get; }
+        public string Phone { get; }
+        public string MobilePhone { get; }
+        public OltPersonName PersonName { get; }
+
+        public List<TestSecurityRoles> Roles => new List<TestSecurityRoles> { TestSecurityRoles.RoleOne, TestSecurityRoles.RoleTwo };
+        public List<TestSecurityPermissions> Permissions => new List<TestSecurityPermissions> { TestSecurityPermissions.PermissionOne, TestSecurityPermissions.PermissionThree };
+
+        public List<Claim> Claims
+        {
+            get
+            {
+                if (_claims.Any())
+                {
+                    return _claims;
+                }
+
+                _claims.Add(new Claim(ClaimTypes.Upn, UserPrincipalName.ToString()));
+                _claims.Add(new Claim(ClaimTypes.NameIdentifier, NameIdentifier));
+                _claims.Add(new Claim(ClaimTypes.GivenName, PersonName.First));
+                _claims.Add(new Claim(OltClaimTypes.MiddleName, PersonName.Middle));
+                _claims.Add(new Claim(ClaimTypes.Surname, PersonName.Last));
+                _claims.Add(new Claim(ClaimTypes.Name, PersonName.FullName));
+                _claims.Add(new Claim(ClaimTypes.Email, Email));
+                _claims.Add(new Claim(ClaimTypes.HomePhone, Phone));
+                _claims.Add(new Claim(ClaimTypes.MobilePhone, MobilePhone));
+                return _claims;
+            }
+        }
+    }
+
+    public class UnitTestingAppIdentity : OltIdentity
+    {
+        public UnitTestingAppIdentity(FakeUserIdentity fakeUser)
+        {
+            FakeUser = fakeUser;
+        }
+
+        public FakeUserIdentity FakeUser { get; }
+
+
+        public override ClaimsPrincipal Identity
+        {
+            get
+            {
+                //var roles = new List<string>();
+                var identity = new GenericIdentity(FakeUser.UserPrincipalName.ToString());
+                FakeUser.Claims.ForEach(claim => identity.AddClaim(claim));
+
+                FakeUser.Roles.ForEach(role =>
+                {
+                    identity.AddClaim(new Claim(ClaimTypes.Role, role.GetCodeEnum()));
+                    //roles.Add(role.GetCodeEnum());
+                });
+                FakeUser.Permissions.ForEach(perm =>
+                {
+                    identity.AddClaim(new Claim(ClaimTypes.Role, perm.GetCodeEnum()));
+                    //roles.Add(perm.GetCodeEnum())
+                });
+
+                return new GenericPrincipal(identity, null);
+            }
+        }
+       
     }
 }
---
+++ OLT.Libraries.UnitTest/OLT.Shared/OltIdentityTests.cs
@@ -0,0 +1,32 @@
+﻿using OLT.Core;
+using OLT.Libraries.UnitTest.Assets.LocalServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OLT.Libraries.UnitTest.OLT.Shared
+{
+    public class OltIdentityTests
+    {
+        [Fact]
+        public void ClaimsTest()
+        {
+            var fakeUser = new FakeUserIdentity();
+            var identity = new UnitTestingAppIdentity(fakeUser);
+
+            Assert.Equal(fakeUser.UserPrincipalName.ToString(), identity.GetClaimTypes(ClaimTypes.Upn)?[0]?.Value);
+            Assert.Equal(fakeUser.NameIdentifier, identity.GetClaimTypes(ClaimTypes.NameIdentifier)?[0]?.Value);
+            Assert.Equal(fakeUser.PersonName.First, identity.GetClaimTypes(ClaimTypes.GivenName)?[0]?.Value);
+            Assert.Equal(fakeUser.PersonName.Middle, identity.GetClaimTypes(OltClaimTypes.MiddleName)?[0]?.Value);
+            Assert.Equal(fakeUser.PersonName.Last, identity.GetClaimTypes(ClaimTypes.Surname)?[0]?.Value);
+            Assert.Equal(fakeUser.Email, identity.GetClaimTypes(ClaimTypes.Email)?[0]?.Value);
+            Assert.Equal(fakeUser.Phone, identity.GetClaimTypes(ClaimTypes.HomePhone)?[0]?.Value);
+            Assert.Equal(fakeUser.Claims.FirstOrDefault(p => p.Type == ClaimTypes.MobilePhone)?.Value, identity.GetClaimTypes(ClaimTypes.MobilePhone)?[0]?.Value);
+
+        }
+    }
+}
