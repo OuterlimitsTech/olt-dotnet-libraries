073de5143979c872225c8f7aa5e1fe55b0bf6c1d
Chris Straw <chrisstraw@users.noreply.github.com>
2022-02-27 21:30:06 -0500

    mods/fix that were lost? (#95)

--- OLT.EF.Core/Interfaces/IOltDbContext.cs
+++ OLT.EF.Core/Interfaces/IOltDbContext.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace OLT.Core
 {
-    public interface IOltDbContext : IOltDbContext<DatabaseFacade>
+    public interface IOltDbContext : IDisposable //IOltDbContext<DatabaseFacade>
     {
+        string DefaultAnonymousUser { get; }
+        string AuditUser { get; }
+        bool ApplyGlobalDeleteFilter { get; }
+        DatabaseFacade Database { get; }
+        int SaveChanges();
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
         DbSet<TEntity> Set<TEntity>() where TEntity : class;
     }
 
--- OLT.Shared/Interfaces/IOltDbContext.cs
+++ OLT.Shared/Interfaces/IOltDbContext.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Threading;
+//using System.Threading.Tasks;
 
-namespace OLT.Core
-{
-    public interface IOltDbContext<out TDatabase> : IDisposable where TDatabase : class
-    {
-        string DefaultAnonymousUser { get; }
-        string AuditUser { get; }
-        bool ApplyGlobalDeleteFilter { get; }
-        TDatabase Database { get; }
-        int SaveChanges();
-        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
-    }
-}
+//namespace OLT.Core
+//{
+//    public interface IOltDbContext<out TDatabase> : IDisposable where TDatabase : class
+//    {
+//        string DefaultAnonymousUser { get; }
+//        string AuditUser { get; }
+//        bool ApplyGlobalDeleteFilter { get; }
+//        TDatabase Database { get; }
+//        int SaveChanges();
+//        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+//    }
+//}
