f2e35f49902270bdfa69d053d105116f7aa54836
Chris Straw <chris.straw@outerlimitstech.com>
2022-03-04 20:57:24 -0500

    moved OltValidationException

--- OLT.Shared/Exceptions/OltValidationException.cs
+++ OLT.Shared/Exceptions/OltValidationException.cs
@@ -1,33 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Runtime.Serialization;
-using System.Security.Permissions;
+﻿////using System;
+////using System.Collections.Generic;
+////using System.Runtime.Serialization;
+////using System.Security.Permissions;
 
-namespace OLT.Core
-{
-    [Serializable]
-    public class OltValidationException : OltException
-    {
-        public readonly IEnumerable<IOltValidationError> Results;
+////namespace OLT.Core
+////{
+////    [Serializable]
+////    public class OltValidationException : OltException
+////    {
+////        public readonly IEnumerable<IOltValidationError> Results;
 
-        public OltValidationException(IEnumerable<IOltValidationError> results, string errorMessage = "Please correct the validation errors") : base(errorMessage)
-        {
-            this.Results = results;
-        }
+////        public OltValidationException(IEnumerable<IOltValidationError> results, string errorMessage = "Please correct the validation errors") : base(errorMessage)
+////        {
+////            this.Results = results;
+////        }
 
-        #region [ Serializable Methods ]
+////        #region [ Serializable Methods ]
 
-        protected OltValidationException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
+////        protected OltValidationException(SerializationInfo info, StreamingContext context) : base(info, context)
+////        {
 
-        }
+////        }
 
-        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
-        public override void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            base.GetObjectData(info, context);
-        }
+////        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+////        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+////        {
+////            base.GetObjectData(info, context);
+////        }
 
-        #endregion
-    }
-}
+////        #endregion
+////    }
+////}
